f78a8c230fbd1a3e199ea5df4b6be1d7d8507568
fubd <312bdf@gmail.com>
2024-05-26 13:32:21 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const count = ref(0)
+function onClick() {
+  count.value = count.value + 1
+}
+
+</script>
+
+<template>
+  <button @click="onClick">click, {{ count }}</button>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
